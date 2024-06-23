d7af4cd6500ef6e47652c13a9c046442af7a88c7
ziadelidrissi <ziadelidrissi1@gmail.com>
2024-06-23 10:00:16 +0200

    feat: data fetched from API

---
+++ src/components/Products.jsx
@@ -0,0 +1,8 @@
+/* eslint-disable react/prop-types */
+function Product({product}) {
+    return(
+        <div>{product.name}</div>
+    )
+}
+
+export default Product;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,55 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Product from "../components/Products";
+
 function HomePage() {
+    const [products, setProducts] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+
+    const getProducts = async () => {
+        try {
+            setIsLoading(true);
+            
+            const response = await axios.get('https://node-api-gngl.onrender.com/products');
+            console.log(response.data);
+
+            setProducts(response.data);
+            setIsLoading(false)
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        getProducts();
+    }, [])
+
     return(
-        <div>This is HomePage</div>
+        <div>
+            <div className="mt-5">
+                {isLoading ? (
+                    'Loading'
+                ) : (
+                    <>
+                    {products.length > 0 ? (
+                        <>
+                        {
+                            products.map((product, index) => {
+                                return(
+                                    <Product key={index} product={product}/>
+                                )
+                            })
+                        }
+                        </>
+                    ) : (
+                        <div>
+                            There is no products
+                        </div>
+                    )}
+                    </>
+                )}
+            </div>
+        </div>
     )
 }
 
